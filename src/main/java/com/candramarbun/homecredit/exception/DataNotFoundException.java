b4573682a5f7b1f99e8e477693133e9d9c5b1a0b
Candra Marbun <candramarbun13@gmail.com>
2020-04-19 09:18:48 +0700

    add exception

---
+++ src/main/java/com/candramarbun/homecredit/exception/DataNotFoundException.java
@@ -0,0 +1,7 @@
+package com.candramarbun.homecredit.exception;
+
+public class DataNotFoundException extends RuntimeException {
+    public DataNotFoundException(String s) {
+        super(s);
+    }
+}
