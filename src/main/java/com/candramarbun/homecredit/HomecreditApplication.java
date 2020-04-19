a27120147638b1faee05b1d713711b652a14fb55
Candra Marbun <candramarbun13@gmail.com>
2020-04-19 07:39:33 +0700

    fresh install

---
+++ src/main/java/com/candramarbun/homecredit/HomecreditApplication.java
@@ -0,0 +1,13 @@
+package com.candramarbun.homecredit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomecreditApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomecreditApplication.class, args);
+	}
+
+}
