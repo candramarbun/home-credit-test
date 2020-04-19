70b1bd9fbd3d4eb8a468b36c090bc37311a842d7
Candra Marbun <candramarbun13@gmail.com>
2020-04-19 10:40:38 +0700

    add update module order feature

--- src/main/java/com/candramarbun/homecredit/controllers/UserController.java
+++ src/main/java/com/candramarbun/homecredit/controllers/UserController.java
@@ -1,12 +1,12 @@
 package com.candramarbun.homecredit.controllers;
 
 import com.candramarbun.homecredit.dto.GeneralResponse;
+import com.candramarbun.homecredit.dto.UserModuleRequest;
 import com.candramarbun.homecredit.services.UserServices;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
@@ -19,7 +19,17 @@ public class UserController {
     }
 
     @GetMapping("/{userId}/modules")
-    public GeneralResponse getModulesByUser(@RequestParam Long userId){
+    public GeneralResponse getModulesByUser(@PathVariable Long userId){
         return GeneralResponse.response(this.userServices.findUserModulById(userId));
+    }
+
+    @PutMapping("/{userId}/update-module")
+    public GeneralResponse updateModule(@PathVariable Long userId,@RequestBody List<UserModuleRequest> requests){
+        try {
+            this.userServices.storeUserModule(userId,requests);
+        } catch (Exception ex){
+            return GeneralResponse.dialog(400,ex.getMessage());
+        }
+        return GeneralResponse.dialog(200,"success");
     }
 }
